f9e4e3cf7f870e8d0c23aeab8cd579430e29f720
Pablirry <pablo.lopez02@csioviedo.com>
2025-02-07 14:59:45 +0100

    DAO Ruta y usuario

---
+++ src/dao/RutaDAO.java
@@ -0,0 +1,72 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import config.ConexionDB;
+
+import modelo.Ruta;
+
+public class RutaDAO {
+
+    public List<Ruta> listarRutas() throws ClassNotFoundException, SQLException {
+        List<Ruta> rutas = new ArrayList<>();
+        try (Connection con = ConexionDB.getConection()) {
+            String sql = "SELECT * FROM rutas";
+
+            try {
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    byte[] imagenBytes = rs.getBytes("imagen");
+                    rutas.add(new Ruta(
+                            rs.getInt("id"),
+                            rs.getString("nombre"),
+                            rs.getString("descripcion"),
+                            imagenBytes,
+                            rs.getDouble("precio"),
+                            rs.getString("dificultad")));
+                }
+            } catch (SQLException e) {
+                System.out.println("Error al listar rutas");
+            }
+
+            return rutas;
+        }
+
+    }
+
+    public void agregarRuta(Ruta ruta) throws ClassNotFoundException {
+        Connection con = ConexionDB.getConection();
+        String sql = "INSERT INTO rutas (nombre, descripcion, imagen, precio, dificultad) VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, ruta.getNombre());
+            ps.setString(2, ruta.getDescripcion());
+            ps.setBytes(3, ruta.getImagen());
+            ps.setDouble(4, ruta.getPrecio());
+            ps.setString(5, ruta.getDificultad());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error al agregar ruta");
+        }
+    }
+
+    public void eliminarRuta(int id) throws ClassNotFoundException {
+        Connection con = ConexionDB.getConection();
+        String sql = "DELETE FROM rutas WHERE id = ?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error al eliminar ruta");
+        }
+    }
+
+}
---
+++ src/dao/UsuarioDAO.java
@@ -0,0 +1,57 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import config.ConexionDB;
+import modelo.Usuario;
+
+public class UsuarioDAO {
+
+    public Usuario iniciarSesion(String correo, String contrasena) {
+        try (Connection con = ConexionDB.getConection()) {
+            String sql = "SELECT * FROM usuario WHERE correo = ? AND contrasena = ?";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, correo);
+            ps.setString(2, contrasena);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                Usuario usuario = new Usuario(rs.getInt("id"),
+                        rs.getString("nombre"),
+                        rs.getString("correo"),
+                        rs.getString("contrasena"),
+                        rs.getString("tipo"),
+                        rs.getBytes("imagenPerfil"));
+                return usuario;
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error al iniciar sesión");
+        }
+        return null;
+    }
+
+    public void registrarUsuario(Usuario usuario) {
+        try (Connection con = ConexionDB.getConection()) {
+            String sql = "INSERT INTO usuario (nombre, correo, contrasena, tipo, imagenPerfil) VALUES (?, ?, ?, ?, ?)";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, usuario.getNombre());
+            ps.setString(2, usuario.getCorreo());
+            ps.setString(3, usuario.getContrasena());
+            ps.setString(4, usuario.getTipo());
+            ps.setBytes(5, usuario.getImagenPerfil());
+
+            ps.executeUpdate();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error al registrar usuario");
+        }
+    }
+
+}
