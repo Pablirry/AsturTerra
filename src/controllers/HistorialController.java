e228aa1544e1ae65bb7a1ce42b8cca755ecbeae9
Pablirry <pablo.lopez02@csioviedo.com>
2025-03-02 11:46:33 +0100

    .

--- src/controllers/HistorialController.java
+++ src/controllers/HistorialController.java
@@ -8,31 +8,29 @@ import java.util.List;
 
 public class HistorialController {
 
-	    private HistorialDAO historialDAO;
-	    private VistaHistorial vistaHistorial;
+	private HistorialDAO historialDAO;
+	private VistaHistorial vistaHistorial;
 
-	    public HistorialController(VistaHistorial vistaHistorial) {
-	        this.historialDAO = new HistorialDAO();
-	        this.vistaHistorial = vistaHistorial;
-	        cargarHistorial();
-	    }
+	public HistorialController(VistaHistorial vistaHistorial) {
+		this.historialDAO = new HistorialDAO();
+		this.vistaHistorial = vistaHistorial;
+		cargarHistorial();
+	}
 
-	    private void cargarHistorial() {
-	        try {
-	            int idUsuario = 1; 
-	            List<Historial> historial = historialDAO.obtenerHistorialUsuario(idUsuario);
-	            DefaultListModel<String> modeloLista = new DefaultListModel<>();
-	            
-	            for (Historial h : historial) {
-	                modeloLista.addElement(h.getFecha() + " - " + h.getAccion());
-	            }
-	            
-	            vistaHistorial.getListaHistorial().setModel(modeloLista);
-	        } catch (Exception e) {
-	            JOptionPane.showMessageDialog(vistaHistorial, "Error al cargar historial: " + e.getMessage());
-	        }
-	    }
-	
+	private void cargarHistorial() {
+		try {
+			int idUsuario = 1;
+			List<Historial> historial = historialDAO.obtenerHistorialUsuario(idUsuario);
+			DefaultListModel<String> modeloLista = new DefaultListModel<>();
 
+			for (Historial h : historial) {
+				modeloLista.addElement(h.getFecha() + " - " + h.getAccion());
+			}
+
+			vistaHistorial.getListaHistorial().setModel(modeloLista);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(vistaHistorial, "Error al cargar historial: " + e.getMessage());
+		}
+	}
 
 }
