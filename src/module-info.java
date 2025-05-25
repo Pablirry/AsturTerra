db7800cfd89269bf3f6d89b2d013b3822103ea4d
Pablirry <pablo.lopez02@csioviedo.com>
2025-05-25 12:04:19 +0200

    .

--- src/module-info.java
+++ src/module-info.java
@@ -3,8 +3,7 @@
  */
 module Turismo {
 	requires java.sql;
-    requires java.desktop;
-    requires java.prefs;
-	requires org.junit.jupiter.api;
+	requires java.desktop;
+	requires java.prefs;
 	
 }
